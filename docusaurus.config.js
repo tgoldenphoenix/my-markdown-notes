ce8b46ac42cc2ed565814ef58dfd5dc53dbebdcd
tgoldenphoenix <anhaophamx@gmail.com>
2025-09-13 08:15:06 +0700

    add search bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,7 +105,29 @@ const config = {
     }),
   markdown: {
     format: 'detect'
-  }
+  },
+  themes: [
+    // ... Your other themes.
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+
+        // For Docs using Chinese, it is recomended to set:
+        // language: ["en", "zh"],
+
+        // Customize the keyboard shortcut to focus search bar (default is "mod+k"):
+        // searchBarShortcutKeymap: "s", // Use 'S' key
+        // searchBarShortcutKeymap: "ctrl+shift+f", // Use Ctrl+Shift+F
+
+        // If you're using `noIndex: true`, set `forceIgnoreNoIndex` to enable local index:
+        // forceIgnoreNoIndex: true,
+      }),
+    ],
+  ],
 };
 
 export default config;
